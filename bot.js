2cf2c354ac3266ad3089a238411c306320f76db3
Niko <bow682@gmail.com>
2021-01-27 22:54:49 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,7 +98,7 @@ client.on('message', message => {
     } else if (command === 'gaycat') {
         let num = 20;
         let i = irandom_range(0,num);
-        twt.get('statuses/user_timeline', {screen_name: args[0], count: num.toString, trim_user: 'true'}, function(error, tweets, response) {
+        twt.get('statuses/user_timeline', {screen_name: 'gayocats', count: num.toString, trim_user: 'true'}, function(error, tweets, response) {
             if(error) throw error;
             message.channel.send(tweets[i].text);
         });
